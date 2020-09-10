247f330784f4f989129d90e4b38fe8cfe892e9b2
Yellucs <21259943+yellucs@users.noreply.github.com>
2020-09-11 00:55:30 +1000

    Merged html using dynamic javascript
    merdged the linuxgen html with the root index dynamically using javascript. Now when you click the Linux button javascript loads the html in the same window.

--- web/assets/js/ini.js
+++ web/assets/js/ini.js
@@ -1,30 +1,31 @@
 function iniLin() {
 
+
+
     var getElemId = [
-        document.getElementById("wrapper"),
+        document.getElementById("status"),
 
     ];
 
+ 
 
     var linuxMarkup = [
         '<h2>Linux Swapfile Generator</h2>',
         '<form action="">',
-        '<input type="radio" name="sizeOption" id="kbOption"> KB',
-        '<input type="radio" name="sizeOption" id="gbOption" checked=""> GB<br>',
-        'Swap File Name and Location: <input type="text" name="swapFileNameLocation" id="swapFileNameLocation" value="/swapfilecode.com"><br>', 
-        'Swap File Size: <input type="number" name="swapFileSize" id="swapFileSize" value="1"><br>',
-        'Swapiness: <input type="number" name="swapiness" id="swapiness" value="20"><br>',
-        'BS: <input type="number" name="bs" id="bs" value="1024"><br>',
+        '<input type="text" name="swapFileNameLocation" id="swapFileNameLocation" value="/swapfilecode.com"> Swap File Name and Location<br>', 
+        '<input type="number" name="swapFileSize" id="swapFileSize" value="1"> Swap File Size<br>',
+        '<input type="number" name="multiSwap" id="multiSwap" value="1"> Swap File Size<br>',
+        '<input type="number" name="swapiness" id="swapiness" value="20"> Swapiness<br>',
+        '<input type="number" name="bs" id="bs" value="1024"> BS<br>',
         '<input type="checkbox" name="isRoot" id="isRootCheckbox"> Are you root?',
         '<input type="checkbox" name="isFallocate" id="isFallocate"> Fallocate',
         '<input type="checkbox" name="isSwapiness" id="isSwapiness"> Swapiness',
-        '<input type="checkbox" name="surviveReboot" id="surviveReboot" checked=""> Survive Reboot<br>',
-        'Code: <br><textarea name="generatorCode" id="generatorCode" rows="14" cols="10" wrap="soft">Choose your settings </textarea><br>',
+        '<input type="checkbox" name="surviveReboot" id="surviveReboot" checked="true"> Survive Reboot<br>',
+        '<br><textarea name="generatorCode" id="generatorCode" rows="14" cols="10" wrap="soft">Choose your settings </textarea><br>',
         '</form>',
-        '<button onclick="linGen()">',
-        'Generate',
-        '</button>'
-        ];
+        '<div id="buttons">',
+        '<button onclick="restDef()"> Restore Default </button>', '<button onclick="linGen()"> Generate </button>', '<button onclick="clearGen()"> Clear </button> </div>'
+       ];
     
 
       
